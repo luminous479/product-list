023f3ac9108d7f85b523fe35e062714f4834d3b6
luminous479 <aklemasultanakhi@gmail.com>
2026-03-11 01:30:26 +0600

    added logger

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/luminous479/product-list/handler"
+	"github.com/luminous479/product-list/middleware"
 	"github.com/luminous479/product-list/router"
 )
 
 func Serve() {
 	mux := http.NewServeMux()
-	mux.Handle("GET /products", http.HandlerFunc(handler.GetProducts))
-	mux.Handle("POST /products", http.HandlerFunc(handler.CreateProductHandler))
-	mux.Handle("GET /products/{productId}", http.HandlerFunc(handler.GetProductByID))
+	mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handler.GetProducts)))
+	mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handler.CreateProductHandler)))
+	mux.Handle("GET /products/{productId}", middleware.Logger(http.HandlerFunc(handler.GetProductByID)))
 	router := router.GlobalRouter(mux)
 
 	// Start the server

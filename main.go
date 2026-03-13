f70aa1d0096e66c231d5656e245fac17850c5b1c
luminous479 <aklemasultanakhi@gmail.com>
2026-03-13 19:00:29 +0600

    base 64 encoding

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"github.com/luminous479/product-list/cmd"
-	
+	"encoding/base64"
+	"fmt"
 )
 
+//"github.com/luminous479/product-list/cmd"
+
 func main() {
-	cmd.Serve()
+	//cmd.Serve()
+	s := "abc"
+	byteArr := []byte(s)
+
+	enc := base64.URLEncoding
+	enc = enc.WithPadding(base64.NoPadding)
+	b64Str := enc.EncodeToString(byteArr)
+    fmt.Println("Byte array : ",byteArr)
+	fmt.Println("Base 64 : ",b64Str)
+
+
 }
